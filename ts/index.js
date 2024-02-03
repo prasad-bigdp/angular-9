a75f8be587cfaa9f68ead3d472ab584e98e1b411
prasad-bigdp <bigdp585@gmail.com>
2024-02-03 10:28:52 +0530

    bahubali

---
+++ ts/index.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+}
+sum("2", "3");
+const sum2 = (a, b) => {
+    return a + b;
+};
+const sub = (a, b) => {
+    return a - b;
+};
+const mul = (a, b) => {
+    return a * b;
+};
+let a = 20;
+export default sum;
+export { sub, mul };
---
+++ ts/index.ts
@@ -0,0 +1,17 @@
+// function sum<T>(a:T, b:T): T
+// {
+//     return a + b
+// }
+// sum<string>("2","3")
+// const sum2 = (a: string, b: string): string => {
+//     return a+b
+// }
+// const sub = (a: number, b: number): number => {
+// 	return a - b
+// }
+// const mul = (a: number, b: number): number => {
+// 	return a * b
+// }
+// let a = 20;
+// export default sum
+// export {sub,mul}
